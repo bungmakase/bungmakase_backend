25aec6d369ea95da6f301e76c8f352d534aa6a3d
InimitablePioneer <seungwonpioneer@gmail.com>
2025-02-16 19:30:37 +0900

    #21 feat: create service and controller

---
+++ src/main/java/swyp_8th/bungmakase_backend/service/BungLevelService.java
@@ -0,0 +1,9 @@
+package swyp_8th.bungmakase_backend.service;
+
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BungLevelService {
+
+}
