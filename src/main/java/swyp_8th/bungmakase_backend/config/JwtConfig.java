b91f24c8f525a607c8b61377027c1a9e2af35330
Doeun Jeong <jde429@khu.ac.kr>
2025-02-18 19:26:48 +0900

    fix: 카카오 로그인 기능 API

---
+++ src/main/java/swyp_8th/bungmakase_backend/config/JwtConfig.java
@@ -0,0 +1,30 @@
+package swyp_8th.bungmakase_backend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Base64;
+import java.util.Date;
+import java.util.UUID;
+
+@Component
+public class JwtConfig {
+    private byte[] SECRET_KEY;
+
+    private static final long EXPIRATION_TIME = 1000L * 60 * 60 * 24 * 30; // 1달
+
+    @Value("${jwt.secret-key}")
+    public void setSecretKey(String secretKey){
+        this.SECRET_KEY = Base64.getDecoder().decode(secretKey);
+    }
+
+    public String generateToken(UUID userId) {
+        return Jwts.builder()
+                .setSubject(String.valueOf(userId))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+}
