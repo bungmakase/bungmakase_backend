052783823e58fb2c730680364dd235ef5202978e
InimitablePioneer <seungwonpioneer@gmail.com>
2025-02-19 16:55:16 +0900

    #20 feat: complete the userInfo method

--- src/main/java/swyp_8th/bungmakase_backend/api/BungLevelController.java
+++ src/main/java/swyp_8th/bungmakase_backend/api/BungLevelController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import swyp_8th.bungmakase_backend.api.dto.UserLevelResponseDto;
+import swyp_8th.bungmakase_backend.exception.UnauthorizedException;
+import swyp_8th.bungmakase_backend.globals.code.FailureCode;
+import swyp_8th.bungmakase_backend.globals.code.SuccessCode;
 import swyp_8th.bungmakase_backend.globals.response.ResponseTemplate;
 import swyp_8th.bungmakase_backend.service.BungLevelService;
 
@@ -14,9 +17,11 @@ import swyp_8th.bungmakase_backend.service.BungLevelService;
 public class BungLevelController {
 
     private final BungLevelService service;
+    private final BungLevelService bungLevelService;
 
-    public BungLevelController(BungLevelService service) {
+    public BungLevelController(BungLevelService service, BungLevelService bungLevelService) {
         this.service = service;
+        this.bungLevelService = bungLevelService;
     }
 
 
@@ -24,18 +29,32 @@ public class BungLevelController {
     public ResponseEntity<ResponseTemplate<UserLevelResponseDto>> getUser(
             @RequestHeader("Authorization") String authorizationHeader
     ) {
+
+        //Format of Authorization header : Bearer JWT_ACCESS_TOKEN
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            ResponseTemplate<UserLevelResponseDto> failResponse =
+                    new ResponseTemplate<>(FailureCode.UNAUTHORIZED_401, null);
+            return ResponseEntity.status(FailureCode.UNAUTHORIZED_401.getCode()).body(failResponse);
+        }
+
         //extract Jwt_Access_Token form "Bearer JWT_ACCESS_TOKEN"
         String token = extractToken(authorizationHeader);
 
+        try {
+            UserLevelResponseDto userLevelResponseDto = bungLevelService.getUserLevel(token);
+            ResponseTemplate<UserLevelResponseDto> response = new ResponseTemplate<>(SuccessCode.SUCCESS_200, userLevelResponseDto);
+            return ResponseEntity.ok(response);
+        } catch (UnauthorizedException ex) {
+            ResponseTemplate<UserLevelResponseDto> failResponse =
+                    new ResponseTemplate<>(FailureCode.UNAUTHORIZED_401, null);
+            return ResponseEntity.status(FailureCode.UNAUTHORIZED_401.getCode()).body(failResponse);
 
+        }
 
-
-
-        return null;
     }
 
     private String extractToken(String header) {
-        if(header != null && header.startsWith("Bearer ")) {
+        if(header != null && header.startsWith("Bearer ")) { //delete this method if the logic duplicated
             return header.substring(7);
         }
         throw new IllegalArgumentException("Invalid Authorization header");
--- src/main/java/swyp_8th/bungmakase_backend/service/BungLevelService.java
+++ src/main/java/swyp_8th/bungmakase_backend/service/BungLevelService.java
@@ -3,12 +3,33 @@ package swyp_8th.bungmakase_backend.service;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.stereotype.Service;
 import swyp_8th.bungmakase_backend.api.dto.UserLevelResponseDto;
+import swyp_8th.bungmakase_backend.domain.Users;
+import swyp_8th.bungmakase_backend.exception.UnauthorizedException;
+import swyp_8th.bungmakase_backend.repository.MyUserRepository;
+import swyp_8th.bungmakase_backend.repository.UserRepository;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class BungLevelService {
 
-    public UserLevelResponseDto getUserLevel(String token) throws Exception {
-        return null;
+    private final MyUserRepository myUserRepository;
+
+    public BungLevelService(MyUserRepository myUserRepository) {
+        this.myUserRepository = myUserRepository;
+    }
+
+    public UserLevelResponseDto getUserLevel(String token) {
+        try {
+            UUID userId = UUID.fromString(token);
+            Users user = myUserRepository.findById(userId)
+                    .orElseThrow(() -> new UnauthorizedException("User not Found"));
+            return new UserLevelResponseDto(user.getNickname(), user.getLevel(), user.getBungCount());
+        } catch (IllegalArgumentException exception) {
+            throw new UnauthorizedException("Invalid token format");
+        }
+
     }
 
 }
