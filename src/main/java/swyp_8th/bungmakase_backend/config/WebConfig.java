568b4feb741cdaa65ea5e80b14778e7996011fe8
Doeun Jeong <jde429@khu.ac.kr>
2025-02-22 16:39:12 +0900

    fix: 쿠키 헤더에 담아 전송

--- src/main/java/swyp_8th/bungmakase_backend/config/WebConfig.java
+++ src/main/java/swyp_8th/bungmakase_backend/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
                         "https://bungmakase.vercel.app", // Vercel 배포 URL
                         "http://localhost:3000"     // React 로컬 개발 서버
                 )
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 쿠키 및 인증 정보를 포함한 요청 허용
     }
