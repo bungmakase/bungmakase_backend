b38cf275b71af70cf8237a76b24db158e54e213c
Doeun Jeong <jde429@khu.ac.kr>
2025-02-26 15:33:55 +0900

    fix: KakaoAuthService

--- src/main/java/swyp_8th/bungmakase_backend/config/CustomClientRegistrationConfig.java
+++ src/main/java/swyp_8th/bungmakase_backend/config/CustomClientRegistrationConfig.java
@@ -8,27 +8,27 @@ import org.springframework.security.oauth2.client.registration.InMemoryClientReg
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 
-@Configuration
-public class CustomClientRegistrationConfig {
-
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        return new InMemoryClientRegistrationRepository(kakaoClientRegistration());
-    }
-
-    private ClientRegistration kakaoClientRegistration() {
-        return ClientRegistration.withRegistrationId("kakao")
-                .clientId("0ed5f2288c7f985d06f7ca5610740c23")
-                .clientSecret("YOUR_CLIENT_SECRET") // 필요시 입력
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_POST)
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .redirectUri("https://bungmakase.shop/api/auth/kakao/callback")
-                .scope("profile", "account_email")
-                .authorizationUri("https://kauth.kakao.com/oauth/authorize")
-                .tokenUri("https://kauth.kakao.com/oauth/token")
-                .userInfoUri("https://kapi.kakao.com/v2/user/me")
-                .userNameAttributeName("id")
-                .clientName("Kakao")
-                .build();
-    }
-}
+//@Configuration
+//public class CustomClientRegistrationConfig {
+//
+//    @Bean
+//    public ClientRegistrationRepository clientRegistrationRepository() {
+//        return new InMemoryClientRegistrationRepository(kakaoClientRegistration());
+//    }
+//
+//    private ClientRegistration kakaoClientRegistration() {
+//        return ClientRegistration.withRegistrationId("kakao")
+//                .clientId("0ed5f2288c7f985d06f7ca5610740c23")
+//                .clientSecret("YOUR_CLIENT_SECRET") // 필요시 입력
+//                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_POST)
+//                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+//                .redirectUri("https://bungmakase.shop/api/auth/kakao/callback")
+//                .scope("profile", "account_email")
+//                .authorizationUri("https://kauth.kakao.com/oauth/authorize")
+//                .tokenUri("https://kauth.kakao.com/oauth/token")
+//                .userInfoUri("https://kapi.kakao.com/v2/user/me")
+//                .userNameAttributeName("id")
+//                .clientName("Kakao")
+//                .build();
+//    }
+//}
