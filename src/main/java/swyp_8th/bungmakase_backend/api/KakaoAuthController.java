1c8fcefac33b1de3a77e05f9280fccaae8a454a3
Doeun Jeong <jde429@khu.ac.kr>
2025-02-26 18:36:39 +0900

    fix: log 추가

--- src/main/java/swyp_8th/bungmakase_backend/api/KakaoAuthController.java
+++ src/main/java/swyp_8th/bungmakase_backend/api/KakaoAuthController.java
@@ -2,8 +2,10 @@ package swyp_8th.bungmakase_backend.api;
 
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import swyp_8th.bungmakase_backend.dto.kakao_auth.KakaoUserInfoDto;
@@ -15,10 +17,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 
+
 @RestController
 @CrossOrigin(origins = {"https://bungmakase.vercel.app", "http://localhost:3000", "https://localhost:3001"})
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
+@Slf4j
 public class KakaoAuthController {
 
     private final KakaoAuthService kakaoAuthService;
@@ -53,23 +57,35 @@ public class KakaoAuthController {
 
     @GetMapping("/kakao/callback")
     public ResponseEntity<Void> kakaoLogin(@RequestParam String code, @RequestParam(required = false) String state) throws IOException{
-        // 카카오 인가코드로 엑세스 토큰 발급
-        String accessToken = kakaoAuthService.getOAuthToken(code, state).getAccessToken();
+        log.info("카카오 로그인 요청: code={}, state={}", code, state);
 
-        // 엑세스 토큰으로 유저 정보 조회 및 저장
-        KakaoUserInfoDto userInfo = kakaoAuthService.getUserInfo(accessToken);
-        String jwtToken = kakaoAuthService.processUserLogin(userInfo);
+        try{
+            // 카카오 인가코드로 엑세스 토큰 발급
+            String accessToken = kakaoAuthService.getOAuthToken(code, state).getAccessToken();
+
+            // 엑세스 토큰으로 유저 정보 조회 및 저장
+            KakaoUserInfoDto userInfo = kakaoAuthService.getUserInfo(accessToken);
+            String jwtToken = kakaoAuthService.processUserLogin(userInfo);
 
 
-        String cookieDomain = "local".equals(state) ? "localhost" : ".vercel.app";
+            String cookieDomain = "local".equals(state) ? "localhost" : ".vercel.app";
 
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.SET_COOKIE, cookieUtill.createCookie(jwtToken, cookieDomain).toString());
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.SET_COOKIE, cookieUtill.createCookie(jwtToken, cookieDomain).toString());
 
-        return ResponseEntity.ok()
-                .headers(headers)
-                .build();
+            log.info("Set-Cookie 헤더 설정 완료: {}", cookieUtill.createCookie(jwtToken, cookieDomain).toString());
+
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .build();
+
+        } catch (Exception e){
+            log.error("카카오 로그인 처리 중 오류 발생", e); //
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
+
 
     }
 }
